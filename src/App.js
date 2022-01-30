60ae98b45197d8bd33354716af7b792f120facfc
ktresel2 <ktreseler2@gmail.com>
2022-01-29 21:44:43 -0500

    Create Nav and first two routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 import './App.css'
 
 const App = () => {
-	return <div className="App">My App</div>
+	return (
+		<Router>
+			<Fragment className="App">
+				<Navbar />
+				<div className="container">
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/about" element={<About />} />
+					</Routes>
+				</div>
+			</Fragment>
+		</Router>
+	)
 }
 
 export default App

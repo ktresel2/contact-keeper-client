91240acc365a34240bfcaf56bdc3d9bb3cf1c841
ktresel2 <ktreseler2@gmail.com>
2022-02-01 07:26:45 -0500

    Add context boiler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+
+import ContactState from './context/contact/contactState'
 import './App.css'
 
 const App = () => {
 	return (
-		<Router>
-			<Fragment className="App">
-				<Navbar />
-				<div className="container">
-					<Routes>
-						<Route exact path="/" element={<Home />} />
-						<Route exact path="/about" element={<About />} />
-					</Routes>
+		<ContactState>
+			<Router>
+				<div className="App">
+					<Navbar />
+					<div className="container">
+						<Routes>
+							<Route exact path="/" element={<Home />} />
+							<Route exact path="/about" element={<About />} />
+						</Routes>
+					</div>
 				</div>
-			</Fragment>
-		</Router>
+			</Router>
+		</ContactState>
 	)
 }
 
---
+++ src/context/contact/contactState.js
@@ -0,0 +1,53 @@
+import React, { useReducer } from 'react'
+import uuid from 'uuid'
+import ContactContext from './contactContext'
+import contactReducer from './contactReducer'
+import {
+	ADD_CONTACT,
+	DELETE_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	UPDATE_CONTACT,
+	FILTER_CONTACTS,
+	CLEAR_FILTER,
+} from '../types'
+
+const ContactState = props => {
+	const initialState = {
+		contacts: [
+			{
+				id: 1,
+				name: 'Joe Smith',
+				email: 'joe@smith.com',
+				phone: '222-2222',
+				type: 'personal',
+			},
+			{
+				id: 2,
+				name: 'Jane Smith',
+				email: 'jane@smith.com',
+				phone: '222-2112',
+				type: 'personal',
+			},
+			{
+				id: 3,
+				name: 'Alex Bradley',
+				email: 'alex@bradley.com',
+				phone: '211-2550',
+				type: 'profesional',
+			},
+		],
+	}
+
+	const [state, dispatch] = useReducer(contactReducer, initialState)
+
+	return (
+		<ContactContext.Provider
+			value={{
+				contacts: state.contacts,
+			}}
+		></ContactContext.Provider>
+	)
+}
+
+export default ContactState

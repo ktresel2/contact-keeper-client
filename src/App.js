57f1a1c95f7734ae05b809ce0b928826d52e1e1c
ktresel2 <ktreseler2@gmail.com>
2022-02-09 07:34:53 -0500

    Add login and register components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
@@ -19,6 +21,8 @@ const App = () => {
 							<Routes>
 								<Route exact path="/" element={<Home />} />
 								<Route exact path="/about" element={<About />} />
+								<Route exact path="/register" element={<Register />} />
+								<Route exact path="/login" element={<Login />} />
 							</Routes>
 						</div>
 					</div>

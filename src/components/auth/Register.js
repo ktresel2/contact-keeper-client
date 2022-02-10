0612ac3dc17054e4b22e97cef433306f7cf4500d
ktresel2 <ktreseler2@gmail.com>
2022-02-09 20:52:25 -0500

    Add alert context

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import AlertContext from './../../context/alert/alertContext'
 
 const Register = () => {
+	const alertContext = useContext(AlertContext)
+
+	const { setAlert } = alertContext
+
 	const [user, setUser] = useState({
 		name: '',
 		email: '',
@@ -16,6 +21,11 @@ const Register = () => {
 
 	const onSubmit = e => {
 		e.preventDefault()
+		if (!name || !email || !password) {
+			setAlert('All fields are required', 'danger')
+		} else if (password !== password2) {
+			setAlert('Passwords do not match', 'danger')
+		}
 		console.log('register submit')
 	}
 
@@ -27,11 +37,23 @@ const Register = () => {
 			<form onSubmit={onSubmit}>
 				<div className="form-group">
 					<label htmlFor="name">Name</label>
-					<input type="text" name="name" value={name} onChange={onChange} />
+					<input
+						type="text"
+						name="name"
+						value={name}
+						onChange={onChange}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="email">Email Address</label>
-					<input type="email" name="email" value={email} onChange={onChange} />
+					<input
+						type="email"
+						name="email"
+						value={email}
+						onChange={onChange}
+						required
+					/>
 				</div>
 				<div className="form-group">
 					<label htmlFor="password">Password</label>
@@ -40,6 +62,8 @@ const Register = () => {
 						name="password"
 						value={password}
 						onChange={onChange}
+						required
+						minLength="6"
 					/>
 				</div>
 				<div className="form-group">
@@ -49,6 +73,7 @@ const Register = () => {
 						name="password2"
 						value={password2}
 						onChange={onChange}
+						required
 					/>
 				</div>
 				<input

f08be94d1791bfba507018866c4677a8398c28ec
ktresel2 <ktreseler2@gmail.com>
2022-02-16 06:50:13 -0500

    Logout and navbar logic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/pages/About'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alerts from './components/layout/Alerts'
+import PrivateRoute from './components/routing/PrivateRoute'
 
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
@@ -28,10 +29,18 @@ const App = () => {
 							<div className="container">
 								<Alerts />
 								<Routes>
-									<Route exact path="/" element={<Home />} />
 									<Route exact path="/about" element={<About />} />
 									<Route exact path="/register" element={<Register />} />
 									<Route exact path="/login" element={<Login />} />
+									<Route
+										exact
+										path="/"
+										element={
+											<PrivateRoute>
+												<Home />
+											</PrivateRoute>
+										}
+									/>
 								</Routes>
 							</div>
 						</div>
